62e27e1e336642c23b83e8555023fa6c2b964d77
risingbirdsong <heregoingthere@outlook.com>
2020-01-31 09:25:54 -0800

    update it on the fly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ChangeEvent } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { TodoComp } from "./components/todoComp";
@@ -9,60 +10,33 @@ import { ConditionalRenderComp } from "./components/conditional";
 import { Z_UNKNOWN } from "zlib";
 import { stringify } from "querystring";
 
-interface AppStateI {
-  item: number;
-  single: string;
-  experiment: any[];
-}
-
-class App extends React.Component<{}, AppStateI> {
+class App extends React.Component<any, any> {
   constructor() {
     super({});
     this.state = {
-      item: 1,
-      single: "default",
-      experiment: []
+      userInput: ""
     };
   }
 
-  singleReturn = (contensArr: any[]) => {
-    return (
-      <div>
-        <hr />
-        <h3>{contensArr[0]}</h3>
-        <p>{contensArr[1]}</p>
-        <hr />
-      </div>
-    );
+  handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      userInput: event.target.value
+    });
   };
 
-  componentDidMount() {
-    let search = "people";
-    let num = 1;
-    fetch(`https://swapi.co/api/${search}/${num}`)
-      .then(resp => resp.json())
-      .then(data => {
-        console.log(data);
-        this.setState({
-          experiment: data
-        });
-      })
-      .then(() => {
-        console.log(this.state.experiment);
-      });
-  }
-
-  transformForDisplay = (holderArr: any[]) => {};
-
   render() {
-    let moreExperiment = { ...this.state.experiment };
-    let arrayFOrObjKeyVal = Object.entries(moreExperiment);
-
-    let mapped = arrayFOrObjKeyVal.map(item => {
-      return this.singleReturn(item);
-    });
-
-    return <div>{mapped}</div>;
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="type here"
+            onChange={this.handleInput}
+          />
+        </form>
+        <p>{this.state.userInput}</p>
+      </div>
+    );
   }
 }
 
